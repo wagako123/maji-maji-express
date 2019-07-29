a3f57465b99341177a679835db791a35c770c114
Brian Wagako <brianwagako@gmail.com>
2019-07-29 16:42:17 +0300

    uniformer

---
+++ Unifromer4/app/src/main/java/com/example/unifromer4/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.unifromer4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    joinNowButton= (Button) findViewById(R.id. main_join_now_btn);
+    loginButton =(Button) findViewById(R.id. main_login_btn);
+
+    }
+}
